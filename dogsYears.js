be4a2bf9a1b7e5a274dd5c48c95c5e86598a11ca
YaraWS <39776511+YaraWS@users.noreply.github.com>
2020-07-28 13:43:56 +0100

    Create dogsYears.js
    Program to convert human years into dog years.

---
+++ dogsYears.js
@@ -0,0 +1,24 @@
+//Age of the user
+const myAge = 31;
+//First Years of a dog life
+let earlyYears = 2;
+//The first two years of a dog’s life count as 10.5 dog years each
+earlyYears *= 10.5;
+//My age subtracted the 2 years of a dogs life 
+let laterYears = myAge -2;
+//Each year following equates to 4 dog years.
+laterYears *= 4;
+//Concatnation of variable earlyYears and laterYears
+let myAgeInDogYears = earlyYears + laterYears;
+//variable name + lowercase method
+const myName = 'Stephen';
+'Stephen'.toLowerCase();
+//Outuput program
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`);
+
+
+
+
+
+
+
